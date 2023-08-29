d58d369c50f85d4cec94329c5f379f822271ecc1
Erratic Engima <erratic-enigma@outlook.com>
2023-08-29 15:56:19 +0100

    Applied 'base' directory to config.

--- vite.config.build.js
+++ vite.config.build.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
+  base: '/qr-code/',
   resolve: {
     alias: [ { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) } ]
   }
